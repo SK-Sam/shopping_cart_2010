5d1b84325e2bc25b067a6b1b4cad6084e719507f
Sam Yeo <yeosamuel95@gmail.com>
2020-11-11 09:03:54 -0700

    Add skeleton test files

---
+++ test/item_test.rb
@@ -0,0 +1,13 @@
+require 'minitest/autorun'
+require './lib/item'
+
+class ItemTest < MiniTest::Test
+
+    def setup
+
+    end
+
+    def test_it_can_instantiate
+
+    end
+end
---
+++ test/vendor_test.rb
@@ -0,0 +1,14 @@
+require 'minitest/autorun'
+require './lib/vendor'
+require './lib/item'
+
+class VendorTest < MiniTest::Test
+
+    def setup
+
+    end
+
+    def test_it_can_instantiate
+
+    end
+end
