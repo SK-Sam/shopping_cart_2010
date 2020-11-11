c4259745499c4ff24f12775ac41e85d746b177ea
Sam Yeo <yeosamuel95@gmail.com>
2020-11-11 09:11:07 -0700

    Add instantiation test for vendor

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -5,10 +5,14 @@ require './lib/item'
 class VendorTest < MiniTest::Test
 
     def setup
-
+        @item1 = Item.new({name: 'Peach', price: "$0.75"})
+        @item2 = Item.new({name: 'Tomato', price: '$0.50'})
+        @vendor = Vendor.new("Rocky Mountain Fresh")
     end
 
     def test_it_can_instantiate
-
+        assert_equal "Rocky Mountain Fresh", @vendor.name
+        expected = {}
+        assert_equal expected, @vendor.inventory
     end
 end
