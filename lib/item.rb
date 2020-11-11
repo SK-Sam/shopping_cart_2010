d160c941f39ff6974968c9ac607fea1521521ebb
Sam Yeo <yeosamuel95@gmail.com>
2020-11-11 09:09:05 -0700

    Add initialization and price method which passes instantiation test

---
+++ lib/item.rb
@@ -0,0 +1,13 @@
+class Item
+    attr_reader :name
+
+    def initialize(description)
+        @name = description[:name]
+        @price = description[:price]
+    end
+
+    def price
+        @price[1..-1].to_f
+    end
+
+end
