b7e585ece2a0765ef025eb8a308b0bc564100ec3
Sam Yeo <yeosamuel95@gmail.com>
2020-11-11 09:05:26 -0700

    Add test for item instantiation

--- test/item_test.rb
+++ test/item_test.rb
@@ -4,10 +4,12 @@ require './lib/item'
 class ItemTest < MiniTest::Test
 
     def setup
-
+        @item1 = Item.new({name: 'Peach', price: "$0.75"})
+        @item2 = Item.new({name: 'Tomato', price: '$0.50'})
     end
 
     def test_it_can_instantiate
-
+        assert_equal "Tomato", @item2.name
+        assert_equal 0.5, @item2.price
     end
 end
