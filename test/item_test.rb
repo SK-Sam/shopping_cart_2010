21b6e263b4717f0304294ef4b098f36966b4c2f0
Sam Yeo <yeosamuel95@gmail.com>
2020-11-11 09:22:37 -0700

    Remove unused var

--- test/item_test.rb
+++ test/item_test.rb
@@ -4,12 +4,11 @@ require './lib/item'
 class ItemTest < MiniTest::Test
 
     def setup
-        @item1 = Item.new({name: 'Peach', price: "$0.75"})
-        @item2 = Item.new({name: 'Tomato', price: '$0.50'})
+        @item = Item.new({name: 'Tomato', price: '$0.50'})
     end
 
     def test_it_can_instantiate
-        assert_equal "Tomato", @item2.name
-        assert_equal 0.5, @item2.price
+        assert_equal "Tomato", @item.name
+        assert_equal 0.5, @item.price
     end
 end
